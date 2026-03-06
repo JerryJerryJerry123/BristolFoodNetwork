6f9a4acb63d8e313234cd877895a1506c7245c21
Harry <harry3.elson@live.uwe.ac.uk>
2026-03-06 00:06:19 +0000

    Docker readded, current working version

--- bristolfoodnetwork/settings.py
+++ bristolfoodnetwork/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'bristolfoodnetwork.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bristol_food_network',
+        'USER': 'root',
+        'PASSWORD': 'django_pass',
+        'HOST': '127.0.0.1',
+        'PORT': '3307',
     }
 }
 
